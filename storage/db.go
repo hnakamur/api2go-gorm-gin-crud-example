03f0a64c65b93bb5a9cffc3097cb4f927cfa62f9
Hiroaki Nakamura <hnakamur@gmail.com>
2015-10-11 08:18:32 +0900

    fmtpolice: Comment the exported function InitDB

--- storage/db.go
+++ storage/db.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+// InitDB creates and migrates the database
 func InitDB() (*gorm.DB, error) {
 	db, err := gorm.Open(os.Getenv("DB_DIALECT"), os.Getenv("DB_PARAMS"))
 	if err != nil {
